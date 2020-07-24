925981bdb8331f4499379c071f83e860c1360c05
DamianGanopolsky <damian98dgg@gmail.com>
2020-07-24 20:38:30 -0300

    Agrego func para calcular la media de f1 a ciertos algos

---
+++ meanf1.py
@@ -0,0 +1,18 @@
+from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
+import pandas as pd
+
+
+def meanf1(list_of_algorithms,x,y):
+    score_total=0
+    i=0
+    
+    for algorithm in list_of_algorithms:
+        
+        scores = cross_val_score(algorithm, x, y, cv=2, scoring='f1_macro')
+        score_total=scores.mean()+score_total
+        i=i+1
+        
+    print(score_total/i)
+    
+
